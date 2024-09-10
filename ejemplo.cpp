dbe6615e6d0706b1697b4f9c59f5c69536599a3f
AVince44 <avince@frsn.utn.edu.ar>
2024-09-10 15:19:26 -0300

    primer comit, tabla de multiplicar

---
+++ ejemplo.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int numero=5;
+
+    for(int i = 1; i <= 10; i++ ){
+        cout <<numero << "x" << i*numero << endl;
+    }
+
+
+
+
+
+
+return 0;
+}
